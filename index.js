bb8b8b4097119a2fd05dcc1e626c0f96dc82f3f6
Zach Ruiz <powersbooth10@gmail.com>
2022-07-25 12:14:45 -0500

    commit tree ee854a469bdcd0a98a7424c63f0a98939f4c3b63

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
